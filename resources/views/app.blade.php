86436ae3081ffd34605710e9e37639445ebae267
Blessing Sanusi <holuwoleh@gmail.com>
2026-03-30 23:34:43 +0100

    add support chatbox

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -50,4 +50,18 @@
     <body class="font-sans antialiased">
         @inertia
     </body>
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/69caf92f8ea3191c37389dfd/1jl0dqoph';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+<!--End of Tawk.to Script-->
 </html>
