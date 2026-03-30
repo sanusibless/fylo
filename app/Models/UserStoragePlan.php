41bb03f42ed9b2af1c4cc4af1017eda7889a7fce
Blessing Sanusi <holuwoleh@gmail.com>
2026-03-30 22:44:41 +0100

    fixed user storage plan

--- app/Models/UserStoragePlan.php
+++ app/Models/UserStoragePlan.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserStoragePlan extends Model
 {
-    protected $storage = ['id'];
+    protected $guarded = ['id'];
 
     public function user()
     {
